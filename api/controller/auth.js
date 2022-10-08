37379e91874acf521359c7f1c360b3a65adb5710
Md. Shaheb Ali <shahebali247bd@gmail.com>
2022-10-08 23:13:02 +0600

    login logout done with context api

--- api/controller/auth.js
+++ api/controller/auth.js
@@ -1,31 +1,87 @@
 import { db } from "../db.js";
 import bcriptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import nodemon from "nodemon";
 
-export const registerController = (res, req) => {
-  const q = "SELECT * from users WHERE email = ? && username = ?";
-  db.query(q, [req.body?.email, req.body?.username], (err, data) => {
-    if (err) return req.json(err);
-    if (data.length > 0) return req.json("User already exists");
-  });
+export const registerController = (req, res) => {
+  try {
+    const q = "SELECT * FROM users WHERE `email`=? OR `username`=?";
 
-  const salt = bcriptjs.genSaltSync(10);
-  const hash = bcriptjs.hashSync(req.body?.password || "Shaheb", salt);
+    db.query(q, [req.body.email, req.body.username], (err, data) => {
+      if (err) return res.json(err.message);
+      if (data.length)
+        return res
+          .status(409)
+          .json("User already exists with the provided username or email");
 
-  const q2 = "INSERT INTO users(`username`, `password`, `email`) VALUES(?)";
-  const vlaues = [req.body?.username, hash, req.body?.email];
-  db.query(q2, [vlaues], (err, data) => {
-    if (err) return res.json(err);
-    if (data.length === 0)
-      return res.json("Registration failed, Can't Insert data into database");
-    if (data.length > 0)
-      return res.status(201).json("Registration Successfull");
-  });
+      const shah = bcriptjs.genSaltSync(10);
+      const hash = bcriptjs.hashSync(req.body.password, shah);
+
+      const q = "INSERT INTO users (username,password,email,img) VALUES(?)";
+
+      const values = [
+        req.body.username,
+        hash,
+        req.body.email,
+        (req.body.img && req.body.img) || "no image provided",
+      ];
+
+      db.query(q, [values], (err, data) => {
+        if (err) return res.json(err.message);
+        const { password, ...others } = values;
+        return res.status(201).json({
+          msg: "Registration Successfull",
+          userInfo: {
+            data: others,
+            status: data,
+          },
+        });
+      });
+    });
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
 };
 
-export const loginController = (res, req) => {
-  req.json({ login: "login" });
+export const loginController = (req, res) => {
+  try {
+    const q = "SELECT * from users WHERE username=?";
+    const vlaues = [req.body.username];
+
+    db.query(q, [vlaues], (err, data) => {
+      if (err) return res.json(err.message);
+      if (data.length === 0) return res.status(404).json("User not found");
+
+      const matchPassword = bcriptjs.compareSync(
+        req.body.password,
+        data[0].password
+      );
+
+      if (!matchPassword) {
+        return res.status(400).json("Wrong username or password");
+      }
+
+      const token = jwt.sign({ id: data[0].id }, "maryamKey");
+      const { password, ...others } = data[0];
+
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .status(200)
+        .json(others);
+    });
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 };
 
-export const logoutController = (res, req) => {
-  req.json("Logout is working");
+export const logoutController = (req, res) => {
+  res
+    .clearCookie("access_token", {
+      sameSite: "none",
+      secure: true,
+    })
+    .status(200)
+    .json("User has been logout successfully");
 };
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -7,8 +7,8 @@ import {
 
 const router = express.Router();
 
-router.get("/login", loginController);
-router.get("/register", registerController);
-router.get("/logout", logoutController);
+router.post("/register", registerController);
+router.post("/login", loginController);
+router.post("/logout", logoutController);
 
 export default router;
